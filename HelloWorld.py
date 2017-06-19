d75fc17204e0b70e92dadeefb0cbd6e98b9136cd
xiaopeng <18971903450@163.com>
2017-06-19 13:40:01 +0800

    add

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,8 @@
-print("hello world")
+p_name = input("\n\nPress input logonName:")
+p_pwd = input("\n\nPress input logonPwd:")
+
+if p_name == "xiaopeng" and p_pwd == "123456":
+    print("true")
+    print("Welcome To Python World!")
+else:
+    print("er")
