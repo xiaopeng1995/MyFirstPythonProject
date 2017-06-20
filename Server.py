d7e2ee23b53c449f77540e8548949cfb60ef40b7
xiaopeng <18971903450@163.com>
2017-06-20 13:49:45 +0800

    add

---
+++ Server.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# 文件名：server.py
+
+# 导入 socket、sys 模块
+import socket
+import sys
+
+# 创建 socket 对象
+serversocket = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM)
+
+# 获取本地主机名
+host = "127.0.0.1"
+
+port = 9999
+
+# 绑定端口
+serversocket.bind((host, port))
+
+# 设置最大连接数，超过后排队
+serversocket.listen(5)
+print("服务启动成功！")
+while True:
+    # 建立客户端连接
+    clientsocket, addr = serversocket.accept()
+
+    print("连接地址: %s" % str(addr))
+
+    msg = '您已成功连接！' + "\r\n"
+    msg += '开始！' + "\r\n"
+    clientsocket.send(msg.encode('utf-8'))
+    clientsocket.send(msg.encode('utf-8'))
+    clientsocket.close()
